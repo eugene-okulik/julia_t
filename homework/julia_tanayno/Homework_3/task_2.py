6735baf9357f58ec69bf14c34219ebb54dce4344
Julua Tanayno <utanayno@gmail.com>
2026-04-20 18:50:20 +0200

    Homework 3 done

---
+++ homework/julia_tanayno/Homework_3/task_2.py
@@ -0,0 +1,6 @@
+x = 3
+y = 5
+
+result = x - y / 5 + x * y
+
+print(result)
---
+++ homework/julia_tanayno/Homework_3/task_4.py
@@ -0,0 +1,10 @@
+cathetus_1 = 10
+cathetus_2 = 14
+
+hypotenuse = (cathetus_1 ** 2 + cathetus_2 ** 2) ** 0.5
+
+print(hypotenuse)
+
+square = (cathetus_1 * cathetus_2) / 2
+
+print(square)
