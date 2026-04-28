b6864373fbc9e83399ea0d02b4de28ca16cd55c8
Julua Tanayno <utanayno@gmail.com>
2026-04-28 13:01:09 +0300

    task 2 updated

--- homework/julia_tanayno/Homework_5/task_1.py
+++ homework/julia_tanayno/Homework_5/task_1.py
@@ -4,15 +4,19 @@ person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
 # распаковка
 name, last_name, city, phone, country = person
 
-# task_2 (вроде решила задачу с помощью среза без index,
-# не сообразила, как тут применить index)
+# task_2
 result_1 = 'результат операции: 42'
 result_2 = 'результат операции: 514'
 result_3 = 'результат работы программы: 9'
 
-new_result_1 = int(result_1[20:]) + 10
-new_result_2 = int(result_2[20:]) + 10
-new_result_3 = int(result_3[28:]) + 10
+index_1 = result_1.index(':') + 2
+new_result_1 = int(result_1[index_1:]) + 10
+
+index_2 = result_2.index(':') + 2
+new_result_2 = int(result_2[index_2:]) + 10
+
+index_3 = result_3.index(':') + 2
+new_result_3 = int(result_3[index_3:]) + 10
 
 print(new_result_1)
 print(new_result_2)
