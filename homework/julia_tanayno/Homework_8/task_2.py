26d0b4eaa151fcb0da8c2eac0e3dbf4fb88f5c40
Julua Tanayno <utanayno@gmail.com>
2026-05-15 08:26:32 +0300

    HW8, task 2 is updated

--- homework/julia_tanayno/Homework_8/task_2.py
+++ homework/julia_tanayno/Homework_8/task_2.py
@@ -6,32 +6,12 @@ def fibonachi():
         yield a
         a, b = b, a + b
 
-# не поняла, как убрать дублирование кода ниже
-# для решения задачи использовала пример с урока 8
-
 
 count = 1
 
-for number in fibonachi():
-    if count == 5:
-        print(number)
-        break
-    count += 1
-
-for number in fibonachi():
-    if count == 200:
-        print(number)
-        break
-    count += 1
-
-for number in fibonachi():
-    if count == 1000:
-        print(number)
-        break
-    count += 1
-
-for number in fibonachi():
-    if count == 100000:
-        print(number)
-        break
-    count += 1
+for expected in [5, 200, 1000, 100000]:
+    for number in fibonachi():
+        if count == expected:
+            print(number)
+            break
+        count += 1
