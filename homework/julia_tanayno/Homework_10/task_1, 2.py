0cf704765c3586961899c910c48868f06e4850e5
Julua Tanayno <utanayno@gmail.com>
2026-05-21 20:11:34 +0200

    Homework 10 done

---
+++ homework/julia_tanayno/Homework_10/task_1, 2.py
@@ -0,0 +1,50 @@
+# task_1
+
+def finish_me(func):
+
+    def wrapper(*args):
+        result = func(*args)
+        print("finished")
+        return result
+    return wrapper
+
+
+@finish_me
+def example(text):
+    print(text)
+
+example("print me1")
+
+
+# task_2.1
+
+def repeat_me(func):
+    def wrapper(*args, **kwargs):
+        count_1 = int(kwargs.get('count'))
+        for i in range(count_1):
+            func(*args)
+    return wrapper
+
+
+@repeat_me
+def example(text):
+    print(text)
+
+example("print me2", count = 2)
+
+# task_2.2
+
+def repeat_me(count):
+    def decorator(func):
+        def wrapper(*args):
+            for i in range(count):
+                func(*args)
+        return wrapper
+    return decorator
+
+
+@repeat_me(count = 2)
+def example(text):
+    print(text)
+
+example("print me3")
